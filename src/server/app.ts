51aa541280f2fb38fc110b40e347e3dfa50be76c
oriolraventosc <oriol.raventos.c@gmail.com>
2022-12-30 16:14:55 +0100

    Create loadTasks functionality

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,9 @@
 import morgan from "morgan";
 import express from "express";
+import cors from "cors";
 import { generalError, unknownEndpoint } from "./middlewares/error.js";
+import routes from "./routes/routes.js";
+import tasksRotuer from "../routers/tasksRouter/tasksRouter.js";
 
 const app = express();
 
@@ -9,6 +12,8 @@ app.disable("x-powered-by");
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use(routes.tasks, cors(), tasksRotuer);
 
 app.use(unknownEndpoint);
 
--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -2,6 +2,7 @@ const routes = {
   tasks: "/tasks",
   addTask: "/add",
   deleteTask: "/delete/:id",
+  loadTasks: "/loadAllTasks",
 };
 
 export default routes;
