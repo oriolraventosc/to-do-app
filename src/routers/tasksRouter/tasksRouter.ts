3e57c4573e72a34e035302358add67fb4249716f
oriolraventosc <oriol.raventos.c@gmail.com>
2022-12-30 16:39:03 +0100

    Create delete functionality

--- src/routers/tasksRouter/tasksRouter.ts
+++ src/routers/tasksRouter/tasksRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   addTask,
+  deleteTask,
   loadAllTasks,
 } from "../../controllers/tasksController/tasksController.js";
 import routes from "../../server/routes/routes.js";
@@ -10,5 +11,6 @@ const tasksRouter = express.Router();
 
 tasksRouter.get(routes.loadTasks, loadAllTasks);
 tasksRouter.post(routes.addTask, addTask);
+tasksRouter.delete(routes.deleteTask, deleteTask);
 
 export default tasksRouter;
